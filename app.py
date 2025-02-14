9a8f359e90b415b46733fb12fec487174c472f55
Abhay kumar <choudharyabhaykumar344@gmail.com>
2025-02-14 14:03:11 +0530

    second commmit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 import jwt
-import os
 
 app = Flask(__name__)
 SECRET_KEY = "supersecret"  # Weak key
@@ -27,4 +26,4 @@ def admin():
         return "Invalid token."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
